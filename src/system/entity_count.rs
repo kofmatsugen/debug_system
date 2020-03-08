c2d2b090226db0d9d3a3c1442a421247caa63557
Kaneyuki Hiromasa <matsugen2511@gmail.com>
2020-03-08 23:48:06 +0900

    entity カウントのデバッグ表示UIをfinderから取得するように変更 debug_entities_count を作成しないと表示されない

--- src/system/entity_count.rs
+++ src/system/entity_count.rs
@@ -1,7 +1,6 @@
-use crate::resource::DebugFont;
 use amethyst::{
     ecs::*,
-    ui::{Anchor, UiText, UiTransform},
+    ui::{UiFinder, UiText},
 };
 
 pub struct EntityCountSystem {
@@ -17,44 +16,30 @@ impl EntityCountSystem {
 }
 
 impl<'s> System<'s> for EntityCountSystem {
-    type SystemData = (
-        ReadExpect<'s, DebugFont>,
-        WriteStorage<'s, UiTransform>,
-        WriteStorage<'s, UiText>,
-        Entities<'s>,
-    );
+    type SystemData = (UiFinder<'s>, WriteStorage<'s, UiText>, Entities<'s>);
 
     fn run(&mut self, data: Self::SystemData) {
-        let (debug_font, mut transforms, mut texts, entities) = data;
+        self.update_ui(data);
+    }
+}
+
+impl EntityCountSystem {
+    fn update_ui(&mut self, data: <EntityCountSystem as System>::SystemData) -> Option<()> {
+        let (finder, mut texts, entities) = data;
+
+        if self.entity_count_ui.is_none() {
+            self.entity_count_ui = finder.find("debug_entities_count");
+        }
+        let ui = self.entity_count_ui?;
 
         let mut count = 0;
         for _ in (&*entities,).join() {
             count = count + 1;
         }
 
-        let system_font = debug_font.system_font.clone();
-        let count_text = format!("entity: {}", count);
+        let text_ui = texts.get_mut(ui)?;
+        text_ui.text = format!("entity: {}", count);
 
-        if let Some(ui) = &self.entity_count_ui {
-            let text = texts.get_mut(*ui).unwrap();
-            text.text = count_text;
-        } else {
-            let entity = entities.create();
-            let transform = UiTransform::new(
-                "entity_count".to_string(),
-                Anchor::TopLeft,
-                Anchor::TopLeft,
-                0.,
-                0.,
-                0.,
-                200.,
-                16.,
-            );
-            let text = UiText::new(system_font, count_text, [0., 0., 0., 1.], 16.);
-
-            let _transform = transforms.insert(entity, transform);
-            let _text = texts.insert(entity, text);
-            self.entity_count_ui = Some(entity);
-        }
+        Some(())
     }
 }
